8eee2daff98a9e16ed64bb81b5182f4b3524d177
uto-usui <nattyanoranged@gmail.com>
2021-03-03 16:54:11 +0900

    ✨ Add data

---
+++ data/info.ts
@@ -0,0 +1,12 @@
+// Immutable site data
+/**
+ * mata elements
+ */
+export const meta = {
+  title: 'Training WebGL',
+  description: ':)',
+  keywords: 'web developer WebGL',
+  ogImg: 'https://uto-usui.com/og.jpg',
+  ogType: 'website',
+  url: 'https://uto-usui.com/',
+} as const
---
+++ data/nav.ts
@@ -0,0 +1,18 @@
+// Immutable site data
+/**
+ * mata elements
+ */
+export const Navigation = {
+  items: [
+    'TECHNOLOGY',
+    'SCIENCE',
+    'HEALTH',
+    'ENVIRONMENT',
+    'TRANSPORT',
+    'POLITICS',
+    'BUSINESS',
+    'CULTURE',
+    'SECURITY',
+    'GEAR',
+  ],
+} as const
